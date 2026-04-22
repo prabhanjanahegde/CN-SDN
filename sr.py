73557d1069de4cec3f5e730d4a0bdaa432a3be67
Prabhanjana P Hegde <prabhanjanahegde@gmail.com>
2026-04-22 12:00:59 +0530

    fix

--- sr.py
+++ sr.py
@@ -3,20 +3,21 @@ import pox.openflow.libopenflow_01 as of
 
 log = core.getLogger()
 
-def install_flow(connection, in_port, dst_ip, out_port):
+def install_rule(connection, dst_ip, out_port):
     msg = of.ofp_flow_mod()
-    msg.match.in_port = in_port
-    msg.match.dl_type = 0x800       # IPv4
+    msg.match.dl_type = 0x800      # IPv4
     msg.match.nw_dst = dst_ip
     msg.actions.append(of.ofp_action_output(port=out_port))
     connection.send(msg)
 
 def _handle_ConnectionUp(event):
-    log.info("Switch %s connected", event.dpid)
+    log.info("Switch connected: %s", event.connection)
 
-    # Example static routes
-    install_flow(event.connection, 1, "10.0.0.2", 2)
-    install_flow(event.connection, 2, "10.0.0.1", 1)
+    # h1 = 10.0.0.1 on port 1
+    # h2 = 10.0.0.2 on port 2
+
+    install_rule(event.connection, "10.0.0.1", 1)
+    install_rule(event.connection, "10.0.0.2", 2)
 
 def launch():
     core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
